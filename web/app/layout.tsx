027115b0c4484d319dfd20efb13e4dd73cfc368c
ecomnazar <ecomnazar.me@gmail.com>
2024-03-01 15:54:07 +0500

    [added]: product cart

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Poppins({
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  style: ['normal']
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,18 +1,20 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    extend: {},
+    colors: {
+      ...colors,
+      primary: "#0E4D65",
+      dark: "#1c2120",
+      grey: "#464646",
     },
   },
   plugins: [],
